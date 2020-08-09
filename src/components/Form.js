89aabe93174804c688155a868639a7411cb13f2e
Brian <blsvsanteliz@gmail.com>
2020-08-09 15:21:08 -0400

    Add value to input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,69 +1,67 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [form, setForm] = useState({
+    name: "",
+    description: "",
+    date: "",
+  });
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const [form, setForm] = useState({
-        name: '',
-        description: '',
-        date: ''
-    })
-    const handleChange = e =>{
-        setForm({
-            ...form,
-            [e.target.name]:e.target.value
-        })
-    }
-
-    const handleSubmit = e =>{
-        e.preventDefault();
-        console.log('submiting ')
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submiting ");
+  };
+  const { date, description, name } = form;
   return (
-      <>
+    <>
       <div className="card-header text-center">
-            <h4>Add task</h4>
+        <h4>Add task</h4>
       </div>
-    <form 
-    className=" card card-body"
-    onSubmit={(e)=>handleSubmit(e)}
-    >
-      <div className="form-group">
-        <label htmlFor="name">Type you task name:</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Name"
-          name="name"
-          id="name"
-          onChange={(e)=>handleChange(e)}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="description">Describe your task:</label>
-        <textarea
-          name="description"
-          id="description"
-          placeholder="Description..."
-          className="form-control"
-          onChange={(e)=>handleChange(e)}
-
-        ></textarea>
-      </div>
-      <div className="form-group">
-        <label htmlFor="date">Date to complete task:</label>
-        <input 
-            type="date" 
-            name="date" 
+      <form className=" card card-body" onSubmit={(e) => handleSubmit(e)}>
+        <div className="form-group">
+          <label htmlFor="name">Type you task name:</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Name"
+            name="name"
+            id="name"
+            onChange={(e) => handleChange(e)}
+            value={name}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Describe your task:</label>
+          <textarea
+            name="description"
+            id="description"
+            placeholder="Description..."
+            className="form-control"
+            onChange={(e) => handleChange(e)}
+            value={description}
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <label htmlFor="date">Date to complete task:</label>
+          <input
+            type="date"
+            name="date"
             id="date"
             className="form-control"
-          onChange={(e)=>handleChange(e)}
-        />
-      </div>
-      <div className="form-group">
-        <button className="btn btn-info btn-block">SUBMIT</button>
-      </div>
-
-    </form>
+            onChange={(e) => handleChange(e)}
+            value={date}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-info btn-block">SUBMIT</button>
+        </div>
+      </form>
     </>
   );
 };
