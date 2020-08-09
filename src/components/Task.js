46e5dfcc9b7c111ed5ffa0d079a893fa7f77de57
Brian <blsvsanteliz@gmail.com>
2020-08-09 19:31:59 -0400

    Add custom style to component Task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,18 @@
 import React from "react";
+import './Task.css'
 
 const Task = ({ task }) => {
     if(task.length === 0 ) return null 
   return (
-      <div className="bg-secondary p-3 mb-2 ">
-        <p>Name: {task.name}</p>
-        <p>Description: {task.description}</p>
-        <p>Date: {task.date}</p>
+      <>
+      <div className="bg text-white p-3 mb-3">
+        <p className=" bold">Name: <span className="normal">{task.name}</span></p>
+        <p className=" bold">Description: <span className="normal">{task.description}</span></p>
+        <p className=" bold">Date: <span className="normal">{task.date}</span></p>
+      <button className="btn btn-danger btn-block ">DELETE &times;</button>
+
       </div>
+</>
   );
 };
 
